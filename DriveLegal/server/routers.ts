a2509189e76dccd1dccf700cb213c2f6ed5f1ac2
ecikan-prog <e.cikan@icloud.com>
2026-07-12 20:05:10 +1200

    Update routers.ts

--- DriveLegal/server/routers.ts
+++ DriveLegal/server/routers.ts
@@ -4,6 +4,12 @@ const t = initTRPC.create();
 
 console.log("ROUTERS FILE LOADED");
 
-export const appRouter = t.router({});
+export const appRouter = t.router({
+  test: t.procedure.query(() => {
+    return {
+      message: "tRPC is working"
+    };
+  }),
+});
 
 export type AppRouter = typeof appRouter;
