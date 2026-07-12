c5ed6d36891fa334797c5e7fedf11547b92aad94
ecikan-prog <e.cikan@icloud.com>
2026-07-12 17:05:02 +1200

    Update routers.ts

--- DriveLegal/server/routers.ts
+++ DriveLegal/server/routers.ts
@@ -1,18 +1,5 @@
 import { router } from "./trpc";
-import { adminRouter } from "./admin";
-import { portalRouter } from "./portal";
-import { excelProtectedRouter } from "./excel-protected";
-import { exportRouter } from "./export-routes";
 
-/**
- * Root TRPC router
- * This is what /api/trpc uses in server/index.ts
- */
-export const appRouter = router({
-  admin: adminRouter,
-  portal: portalRouter,
-  excel: excelProtectedRouter,
-  export: exportRouter,
-});
+export const appRouter = router({});
 
 export type AppRouter = typeof appRouter;
