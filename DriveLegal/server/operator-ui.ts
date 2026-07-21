9276f8ac0eba69c110e2015e62e81886b9e0c338
ecikan-prog <e.cikan@icloud.com>
2026-07-21 21:06:44 +1200

    Update operator-ui.ts

--- DriveLegal/server/operator-ui.ts
+++ DriveLegal/server/operator-ui.ts
@@ -1,1 +1,7 @@
+import { Express, Request, Response } from "express";
 
+export function registerOperatorUi(app: Express) {
+  app.get("/operator/login", (_req: Request, res: Response) => {
+    return res.status(200).send("Operator Portal");
+  });
+}
