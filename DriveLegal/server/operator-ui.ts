d227b0cea286df8c4f9233dae63ffa684243453d
ecikan-prog <e.cikan@icloud.com>
2026-07-22 20:55:15 +1200

    Delete DriveLegal/server/operator-ui.ts

--- DriveLegal/server/operator-ui.ts
+++
@@ -1,7 +0,0 @@
-import { Express, Request, Response } from "express";
-
-export function registerOperatorUi(app: Express) {
-  app.get("/operator/login", (_req: Request, res: Response) => {
-    return res.status(200).send("Operator Portal");
-  });
-}
